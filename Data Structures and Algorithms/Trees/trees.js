5ad844ebd10ce74eb044de5ff41181b1051be0bb
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-03-19 22:59:16 -0500

    Add tree class

--- Data Structures and Algorithms/Trees/trees.js
+++ Data Structures and Algorithms/Trees/trees.js
@@ -1,9 +1,16 @@
+// Trees
 
-
+// 
 class TreeNode {
     constructor(data, left, right) {
         this.data = data;
         this.left = left;
         this.right = right;
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null;
     }
 }
