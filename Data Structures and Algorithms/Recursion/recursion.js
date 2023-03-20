3d5cea200f5acbcf14efd51eb30384429e01ffe9
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-03-20 03:18:30 -0500

    finalize v2 palindrome function

--- Data Structures and Algorithms/Recursion/recursion.js
+++ Data Structures and Algorithms/Recursion/recursion.js
@@ -90,5 +90,18 @@ function v2Palindrom(str, start = 0, end = null) {
     if (end === null) {
         end = str.length - 1;
     }
-    
+    if (str.length < 2) {
+        return true;
+    } else if (end <= start) {
+        return true;
+    } else if (str.charAt(start) !== str.charAt(end)) {
+        return false;
+    } else {
+       return v2Palindrom(str, start + 1, end - 1 )
+    }
 }
+
+console.log('\nVersion 2 Palindrome:')
+console.log('Testing the string "d": ' + v2Palindrom('d'))
+console.log('Testing the string "saippuakivikauppias": ' + v2Palindrom('saippuakivikauppias'))
+console.log('Testing the string "saippuakivioauppias": ' + v2Palindrom('saippuakivioauppias'))
