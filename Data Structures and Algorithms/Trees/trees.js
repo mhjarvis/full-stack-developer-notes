44b74739888fdc09e397e544fc52fef6a425b90a
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-03-19 23:28:09 -0500

    semi-complete collect function

--- Data Structures and Algorithms/Trees/trees.js
+++ Data Structures and Algorithms/Trees/trees.js
@@ -16,12 +16,23 @@ class Tree {
 
     // Visit every node in the tree, and add the value to the array
     collect() {
-
+        this._collect(this.root, []);
     }
     
-    // Underscore indicates people should not use this function themselves
-    _collect(result = []) {
+    // Underscore indicates people should not use this function themselves. This simulates a private function which
+    // should only be called from the Tree class itself. People should use the 'collect' function which calls this one.
+    _collect(node, result) {
+        if (node === null) {
+            return result;
+        }
+        // Add current node to the array
+        result.push(node.data);
 
+        // Recurse left and right
+        this._collect(node.left, result);
+        this._collect(node.right, result);
+
+        return result;
     }
 }
 
