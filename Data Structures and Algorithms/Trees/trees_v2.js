f29f123244027d9f6d1b880be982c3070c18d0c1
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-03-20 23:10:25 -0500

    add search types notes

---
+++ Data Structures and Algorithms/Trees/trees_v2.js
@@ -0,0 +1,8 @@
+// Two types of search related to trees:
+
+// Depth-First-Search - starts at the initial node and goes deepter into the tree until it reaches
+// an element without children - a leaf. Then it backtracks and visits the most recent node
+// that has not yet been explored.
+
+// Breadth-First Search - starts from the root node and first traverses all the neighboring nodes.
+// Then it selects the nearest node and explores the new nodes. 
