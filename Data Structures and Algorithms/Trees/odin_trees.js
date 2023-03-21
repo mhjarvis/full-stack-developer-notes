87650be282e35433451657fd7579da535c97da8e
Markus <markus.h.jarvis@gmail.com>
2023-03-21 14:02:31 -0500

    rework

--- Data Structures and Algorithms/Trees/odin_trees.js
+++ Data Structures and Algorithms/Trees/odin_trees.js
@@ -2,10 +2,10 @@
 // stores ass well as its left and right children;
 
 class Node {
-    constructor(data, left = null, right = null) {
+    constructor(data) {
         this.data = data;
-        this.left = left;
-        this.right = right;
+        this.left = null;
+        this.right = null;
     }
 }
 
@@ -14,66 +14,9 @@ class Node {
 // which you'll write next.
 
 class Tree {
-    constructor(arr = []) {
-        this.root = this.buildTree(arr);
+    constructor() {
+        this.root = null;
     }
 
-    // Function that builds the actual tree
-    buildTree(arr) {
-        arr = arr.sort(function(a, b){return a - b});           // sort array
-        arr = [...new Set(arr)];                                // remove duplicates
 
-        if(arr.length === 0) {
-            return null;
-        }
-
-        const mid = Math.floor(arr.length / 2);
-        const node = new Node(arr[mid]);
-
-        node.left = this.buildTree(arr.slice(0, mid));
-        node.right = this.buildTree(arr.slice(mid + 1));
-        return node;       
-    }
-
-    // Function for inserting a new node
-    insert(value, currentNode = this.root) {
-        if(currentNode == null) {
-            currentNode = new Node(value);
-            return currentNode;
-        }
-        if(value < currentNode.data) {
-            currentNode.left = this.insert(value, currentNode.left);
-        } else {
-            currentNode.right = this.insert(value, currentNode.right);
-        }
-        return currentNode;
-    }
 }
-
-// Function to print the tree layout
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node === null) {
-        return;
-    }
-    if (node.right !== null) {
-        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-    if (node.left !== null) {
-        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    }
-}
-
-console.log("\n...building tree...\n")
-
-let tree = new Tree([4, 66, 24, 5, 100, 48, 24, 88, 74, 25, 64, 19])
-
-console.log(prettyPrint(tree.root))
-console.log('\n...Inserting the numbers 65, 34, and 2...\n')
-
-tree.insert(65);
-tree.insert(34);
-tree.insert(2);
-
-console.log(prettyPrint(tree.root))
-
