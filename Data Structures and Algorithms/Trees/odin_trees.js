3b135ea8342b4473091412e3c9dd08cedcc10826
Markus <markus.h.jarvis@gmail.com>
2023-03-21 11:11:53 -0500

    add prettyPrint()

--- Data Structures and Algorithms/Trees/odin_trees.js
+++ Data Structures and Algorithms/Trees/odin_trees.js
@@ -18,20 +18,39 @@ class Tree {
         this.root = this.buildTree(arr);
     }
 
-    // Function to sort and remove duplicates of array. 
+    // Function that builds the actual tree
     buildTree(arr) {
-        arr = [...new Set(arr.sort(function(a, b){return a - b}))];
+        arr = arr.sort(function(a, b){return a - b});
+        arr = [...new Set(arr)];
 
         if(arr.length === 0) {
             return null;
         }
 
-        const mid = arr[parseInt(arr.length / 2)];
+        const mid = Math.floor(arr.length / 2);
+        const node = new Node(arr[mid]);
 
-        console.log(mid);
-        console.log(arr);
-        
+        node.left = this.buildTree(arr.slice(0, mid));
+        node.right = this.buildTree(arr.slice(mid + 1));
+        return node;       
+    }
+
+}
+
+// Function to print the tree layout
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
 }
 
+
 let tree = new Tree([4, 66, 24, 5, 100, 48, 24, 88, 74, 25, 64, 19])
+console.log(prettyPrint(tree.root))
