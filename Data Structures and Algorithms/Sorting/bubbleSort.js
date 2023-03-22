8127e7a7fcaf488fd341a996cfb32226d87b3ab3
Markus <markus.h.jarvis@gmail.com>
2023-03-22 17:26:04 -0500

    Add non-optimized solution

--- Data Structures and Algorithms/Sorting/bubbleSort.js
+++ Data Structures and Algorithms/Sorting/bubbleSort.js
@@ -2,14 +2,21 @@
 let arr = [34, 22, 39, 4, 18, 384, 2 ,3, 298, 6, 43, 19, 29, 38]
 
 function bubbleSort(arr) {
-    let a = 0;
-    let b = 1;
 
-    for(let i = 0; i < arr.length - 1; i++) {
-        for(let j = 0; j < arr.length - 1; j++) {
-            if()
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr.length; j++) {
+            if(arr[j] > arr[j + 1]) {
+                let temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
         }
     }
+    return arr;
+}
+
+function bubbleSortOptimized(arr) {
+    
 }
 
 console.log('\nBubble Sort\n')
