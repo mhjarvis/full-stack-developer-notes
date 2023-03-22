9c6dc3686adfdeee684cb6d809abba514c3a3863
Markus <markus.h.jarvis@gmail.com>
2023-03-22 18:57:22 -0500

    Add notes

--- Data Structures and Algorithms/Sorting/bubbleSort.js
+++ Data Structures and Algorithms/Sorting/bubbleSort.js
@@ -1,4 +1,27 @@
+/*  Efficiency of Bubble Sort
 
+Bubble sort contains both comparisons and swaps. 
+
+The number of comparisons that happen will reduce by 1 after each pass through (the final number is always in the correct spot). For a array with 5 elements, we would thus have:
+
+    4 + 3 + 2 + 1 = 10 comparisons
+
+In a more formal way, we could say that for an array of all sizes we have:
+
+    (N - 1) + (N - 2) + (N - 3)... + 1 comparisons
+
+The number of swaps that happen will vary, but in a worst case scenario (the array is descending), we would perform a swap during each comparison. In this case, as the number of elements in the array increases, the number of swaps will also most likely increase:
+
+N Data Elements         Max # of Steps
+        5                     20
+        10                    90
+        20                    380
+        40                    1560
+        80                    6320
+
+Based on the increase in steps, you can see that the growth is about equal to N^2.
+
+*/
 let arr = [34, 22, 39, 4, 18, 384, 2 ,3, 298, 6, 43, 19, 29, 38]
 let arr2 = [34, 22, 39, 4, 18, 384, 2 ,3, 298, 6, 43, 19, 29, 38]
 
