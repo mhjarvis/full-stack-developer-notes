f575e9156c90fc8243ccc6964c666f2488366a29
Markus <markus.h.jarvis@gmail.com>
2023-03-24 12:27:55 -0500

    Add insertionSort() to output

--- Data Structures and Algorithms/Sorting/compareTheSortAlgorithms.js
+++ Data Structures and Algorithms/Sorting/compareTheSortAlgorithms.js
@@ -51,6 +51,25 @@ function selectionSort(array) {
     return array;
 }
 
+// INSERTION SORT
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++) {
+        let temp = arr[i]       
+        let position = i - 1
+
+        while(position >= 0) {
+            if(arr[position] > temp) {
+                arr[position + 1] = arr[position]
+                position = position - 1
+            } else {
+                break
+            }
+        }
+        arr[position + 1] = temp
+    }
+    return arr
+}
+
 let arr = [
     7871,   32, 1890, 3489, 7715,  183, 8765, 2867, 5188, 6962,
      186, 7278, 9768, 7617, 9479, 5587, 2983, 1111, 9040, 9028,
@@ -89,6 +108,19 @@ let arr3 = [
     1090,  313,  109, 6680, 4176, 9586, 6641, 3965, 2143, 3216,
     9957, 6947, 8768, 2252, 1071, 1057, 3323, 3877,  312, 3056
   ]
+
+let arr4 = [
+    7871,   32, 1890, 3489, 7715,  183, 8765, 2867, 5188, 6962,
+     186, 7278, 9768, 7617, 9479, 5587, 2983, 1111, 9040, 9028,
+    3130,  825, 5478, 8982, 1649, 7504, 7840, 8521, 8833, 6067,
+    1761,   52, 5774, 1402, 7981, 9554,  432, 5523, 7667, 6745,
+     137, 2770, 5347, 1186, 9004, 9289,  706, 2566,  305, 2515,
+    6351, 1490, 3287, 3681, 9337, 2941, 4532, 6672,  399, 8250,
+     157, 1045, 4515, 9096, 1555, 2129, 5143,  409,  669, 1833,
+    1610,  730, 2068, 3400, 9650, 2889, 3169, 7836,  729, 9917,
+    1090,  313,  109, 6680, 4176, 9586, 6641, 3965, 2143, 3216,
+    9957, 6947, 8768, 2252, 1071, 1057, 3323, 3877,  312, 3056
+  ]
   
 
 
@@ -107,11 +139,17 @@ selectionSort(arr3);
 let end3 = performance.now();
 let time3 = end3 - start3;
 
+let start4 = performance.now();
+selectionSort(arr4);
+let end4 = performance.now();
+let time4 = end4 - start4;
 
 
-console.log('Execution time for bubbleSort(): ' + time1);
-console.log('Execution time for bubbleSortOptimized(): ' + time2);
-console.log('Execution time for selectionSort():' + time3);
+
+console.log('Execution time for bubbleSort(): ' + time1 * 100 + ' ms');
+console.log('Execution time for bubbleSortOptimized(): ' + time2 * 100 + ' ms');
+console.log('Execution time for selectionSort():' + time3 * 100 + ' ms');
+console.log('Execution time for insertionSort(): ' + time4 * 100 + ' ms');
 
 /* function test(n) {
     let arr = [];
---
+++ Data Structures and Algorithms/Sorting/insertionSort.js
@@ -0,0 +1,39 @@
+/* Efficiency of Insertion Sort
+
+Logic of Insertion Sort:
+
+1. Loop through the array starting at index 1.
+2. Assign the value at array[i] to a temporary value.
+3. Create a position variable and assign it the value of index - 1. The position 
+   variable will represent the value we compare against the temporary value.
+4. Create a inner while loop which runs while position is greater than or equal
+   to 0.
+5. Compare position to temporary value.
+   a. If position is greater shift that value to the right
+   b. Decrement position by 1.
+   c. If we encounter a value at position that is greater or equal to the temporary
+      value, we can end our pass-through.
+6. Move the temp value in the new gap.
+
+*/
+
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++) {
+        let temp = arr[i]       
+        let position = i - 1
+
+        while(position >= 0) {
+            if(arr[position] > temp) {
+                arr[position + 1] = arr[position]
+                position = position - 1
+            } else {
+                break
+            }
+        }
+        arr[position + 1] = temp
+    }
+    return arr
+}
+
+let arr = [34, 22, 39, 4, 18, 384, 2 ,3, 298, 6, 43, 19, 29, 38]
+console.log(insertionSort(arr))
