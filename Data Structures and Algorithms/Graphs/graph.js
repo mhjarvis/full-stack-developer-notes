165867b3d9575dc21e9cfe95ec1cd47f7a289449
Markus <markus.h.jarvis@gmail.com>
2023-03-27 10:07:02 -0500

    add hash table

--- Data Structures and Algorithms/Graphs/graph.js
+++ Data Structures and Algorithms/Graphs/graph.js
@@ -1,4 +1,4 @@
-/* 
+/* General Information on Graphs
 
 Graph data structures are good at showing relationships between data. 
 
@@ -11,5 +11,25 @@ Graph data structures are good at showing relationships between data.
 Each item is represente by a node connected to each other. While this may look like a tree, the main difference to keep in mind is that all trees are graphs, but not all graphs are trees. For a graph to be considered a tree, it cannot have cycles (for example, Alice -> Diana -> Bob -> Diana), and all nodes must be connected (it is possible in graphs that nodes are not connected).
 
 Nodes are a "vertex" in graphs and the lines (vertices) between nodes are "edges". Vertices that are connected by an edge are said to be "adjacent" to each other (also called "neighbors"). A graph withall of the vertices connected in some way is called a "connected graph". 
+
+*/
+
+// Implement basic has table of the above relationships
+
+let friends = {
+    'Alice': ['Bob', 'Diana', 'Fred'],
+    'Bob' : ['Alice', 'Cynthia', 'Diana'],
+    'Cynthia': ['Bob'],
+    'Diana': ['Alice', 'Bob', 'Fred'],
+    'Elise': ['Fred'],
+    'Fred': ['Alice', 'Diana', 'Elise'],
+}
+
+console.log(friends.Alice)
+
+/* Direct Graphs 
+
+
+
 
 */
