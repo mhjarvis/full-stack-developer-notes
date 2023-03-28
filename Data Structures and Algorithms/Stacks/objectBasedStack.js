0fbff3e247ca270bcf0edfd676fccee61e907d33
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-03-27 22:54:35 -0500

    add output code to show pushing elements

--- Data Structures and Algorithms/Stacks/objectBasedStack.js
+++ Data Structures and Algorithms/Stacks/objectBasedStack.js
@@ -1,5 +1,7 @@
 /* Object-Based Stack
 
+In JavaScript, an object is a set of key and value pairs. The 'count' variable will be the key of the objects. 
+
 */
 
 // instantiate class
@@ -9,5 +11,20 @@ class Stack {
         this.items = {};
     }
 
-    // 
+    // add elements to the stack (can add multiple elements at a time)
+    push(element) {
+        this.items[this.count] = element;
+        this.count++;
+    }
 }
+
+// instantiate a stack and see if it is empty...
+const stack = new Stack();
+
+// add some elements to the stack using push...
+stack.push(5);
+stack.push(10);
+stack.push(22);
+stack.push(23);
+
+console.log('\nAdded the numbers 5, 10, 22, 23 and printing the current stack: ', stack);
