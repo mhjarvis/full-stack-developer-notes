978c5fb57dea62dd2b35f86e97d4394f3bf3000a
Markus <markus.h.jarvis@gmail.com>
2023-03-27 19:22:33 -0500

    Add initial notes

---
+++ Data Structures and Algorithms/Stacks/stack.js
@@ -0,0 +1,34 @@
+/* Stacks
+
+Stacks are last in first out (LIFO) data structures. 
+
+Top - indicates the top of the stack.
+Bottom - indicates the bottom of the stack.
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+// function to push an item to the top of the stack
+
+
+// function to remove an item from the top of the stack
+
+
+// function that shows the last item pushed into the stack
+
+
+// function that empties the stack
+
+
+// function to get current size of the stack
