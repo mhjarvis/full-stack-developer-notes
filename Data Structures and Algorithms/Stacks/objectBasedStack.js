f9b0aeb0474d0da44fdb0174c1cb4794a8885730
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-03-27 22:48:45 -0500

    Initialize file

---
+++ Data Structures and Algorithms/Stacks/objectBasedStack.js
@@ -0,0 +1,13 @@
+/* Object-Based Stack
+
+*/
+
+// instantiate class
+class Stack {
+    constructor() {
+        this.count = 0;             // keep track of size of array
+        this.items = {};
+    }
+
+    // 
+}
