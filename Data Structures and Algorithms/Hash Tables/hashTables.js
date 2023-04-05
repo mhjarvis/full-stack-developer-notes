121aafe41d4a67de574a36c7f396ed24205ff0af
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-04-04 22:38:54 -0500

    update notes

--- Data Structures and Algorithms/Hash Tables/hashTables.js
+++ Data Structures and Algorithms/Hash Tables/hashTables.js
@@ -1,5 +1,7 @@
 /* Hash Tables
 
+Depending on the language, these are refered to by many different names (e.g. maps, hash maps, dictionaries, associative arrays, etc.). Hash Table is a list of paired values. The first item is the 'key' and the second item is the 'value'.
+
 Efficiency:
 
 Hash Tables can be used to look up data in O(1) time.
