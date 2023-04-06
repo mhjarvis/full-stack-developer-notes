b90eacf5302ee92979568ee38cf6d868e53f2d4e
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-04-05 23:07:08 -0500

    Update initial

---
+++ Data Structures and Algorithms/Recursion/recursion.js
@@ -0,0 +1,12 @@
+/* Recursion
+
+Recursion is a function calling itself. For example, the following function will call itself...indefinetly:
+
+    function countdown(number) {
+        console.log(number);
+        countdown(number - 1);
+    }
+
+THE BASE CASE
+
+*/
