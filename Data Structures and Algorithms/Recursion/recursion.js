6dc473f57444f257e3a27506e3ad961e95ac482b
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-04-05 23:11:33 -0500

    Update recursion notes

--- Data Structures and Algorithms/Recursion/recursion.js
+++ Data Structures and Algorithms/Recursion/recursion.js
@@ -9,6 +9,12 @@ Recursion is a function calling itself. For example, the following function will
 
 THE BASE CASE
 
-To prevent our function from continuing indefinetly, we need a way to end the function calling itself. This is the base case - usually a conditional statement that will stop the function calling.
+To prevent our function from continuing indefinetly, we need a way to end the function calling itself. This is the base case - usually a conditional statement that will stop the function calling. For the above function we could add...
+
+    if(number === 0) return;
+
+Thus, the ```base case``` is the case in the function that the function will not recurse. EVERY RECURSIVE FUNCTION NEEDS AT LEAST ONE BASE CASE TO PREVENT IT FROM CALLING ITSELF INDEFINITELY.
+
+
 
 */
