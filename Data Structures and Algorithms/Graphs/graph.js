39e15c77aacf57a468bbcefba7cd643284938b93
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-04-26 10:05:47 -0500

    Update graph notes

--- Data Structures and Algorithms/Graphs/graph.js
+++ Data Structures and Algorithms/Graphs/graph.js
@@ -1,19 +1,42 @@
-/*
+/*  Graphs
 
 Graphs = Nodes (vertices) + Edges. There are directed and non-directed graphs. 
 
+In graphs, you can also have depth-first and breadth-first traversals.
 
-
-
-
-
-
+1. Depth-First Traversal - exploring one direction as far as you can before switching directions. 
+    a. Uses a Stack
+2. Breadth-First Traversal - from starting point, explore all immediate neighbors to the first node, then follow that behavior. Explore all directions equally. 
+    b. Uses a Queue
 
 */
 
+// Basic graph using an adjacency list
+const graph = {
+    a: ['b', 'c'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: []
+}
 
+// Depth-First Function
+const depthFirstPrint = (graph, source) => {
+    const stack = [source];                                 // create a stack that holds each node and starts 
+                                                            // with the source node
+    while(stack.length > 0) {                               // do things while the stack has items in it
+        const current = stack.pop();                        // remove the first node for processing
+        console.log(current);                               // print out the value
+        for (let neighbor of graph[current]) {          
+            stack.push(neighbor);                           // add each neighbor to the stack (array)
+        }
+    }
+}
 
+depthFirstPrint(graph, 'a');        // abcdfce
 
+//
 
 
 
