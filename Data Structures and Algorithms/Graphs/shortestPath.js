1da336d304b8638c3c9852646f51729fae0f3db9
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-04-27 16:09:25 -0500

    initialize new file

---
+++ Data Structures and Algorithms/Graphs/shortestPath.js
@@ -0,0 +1,24 @@
+/*
+
+
+
+*/
+
+const edges = [
+    ['w', 'x'],
+    ['x', 'y'], 
+    ['z', 'y'],
+    ['z', 'v'],
+    ['w', 'v']
+]
+
+const toGraph = (edges) => {
+    const graph = {};           // create graph object to hold adjacency list
+
+
+
+
+    return graph;
+}
+
+console.log(toGraph(edges));
