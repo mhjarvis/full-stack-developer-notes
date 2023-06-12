8011483222934014b3499ec61d0fcddcbacabf23
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-06-12 09:50:07 -0500

    update

---
+++ Test Driven Development (TDD)/Odin_Work/odin_testing_practice.js
@@ -0,0 +1,41 @@
+function sum(a, b) {
+    return a + b;
+}
+
+
+// A capitalize function that takes a string and returns it with the first 
+// character capitalized.
+
+function capitalizeFirstLetter(str) {
+
+    return str[0].toUpperCase() + str.slice(1)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = sum;
+
