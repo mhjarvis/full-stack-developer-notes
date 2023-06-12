05ead3355c14feccc4f895a1e3fb23c38e5d76b0
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-06-12 15:30:36 -0500

    finalize test array to pass

--- Test Driven Development (TDD)/Odin_Work/analyzeArray.js
+++ Test Driven Development (TDD)/Odin_Work/analyzeArray.js
@@ -1,9 +1,23 @@
 function analyzeArray(arr) {
 
+    let obj = {};
+    
+    obj.average = getAverage(arr);
+    obj.min = Math.min(...arr);
+    obj.max = Math.max(...arr);
+    obj.length = arr.length;
 
+    return obj;
+}
 
-
-
+function getAverage(arr) {
+    let average = 0;
+    for(let i = 0; i < arr.length; i++) {
+        average += arr[i];
+    }
+    return Math.floor(average / (arr.length));
 }
 
 module.exports = analyzeArray;
+
+console.log(analyzeArray([1, 8, 3, 4, 2, 6]));
--- Test Driven Development (TDD)/Odin_Work/analyzeArray.test.js
+++ Test Driven Development (TDD)/Odin_Work/analyzeArray.test.js
@@ -2,9 +2,9 @@ const analyzeArray = require('./analyzeArray');
 
 test('arr returns object with correct values', () => {
     expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
-        average: 3,
+        average: 4,
         min: 1,
-        max: 5,
-        length: 5,
+        max: 8,
+        length: 6,
     })
 })
