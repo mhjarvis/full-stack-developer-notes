0e046b6692e4852372547da1095dce7177ff06fb
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-06-12 10:59:43 -0500

    add calculator testing

--- Test Driven Development (TDD)/Odin_Work/calculator.test.js
+++ Test Driven Development (TDD)/Odin_Work/calculator.test.js
@@ -3,3 +3,15 @@ const calculator = require('./calculator');
 test('adding 2 + 3 equals 5', () => {
     expect(calculator.add(2, 3)).toBe(5);
 })
+
+test('subtracting 2 - 3 equals -1', () => {
+    expect(calculator.add(2, 3)).toBe(-1);
+})
+
+test('adding 2 + 3 equals 5', () => {
+    expect(calculator.add(2, 3)).toBe(5);
+})
+
+test('adding 2 + 3 equals 5', () => {
+    expect(calculator.add(2, 3)).toBe(5);
+})
