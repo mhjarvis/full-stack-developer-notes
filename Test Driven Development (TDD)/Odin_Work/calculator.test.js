404dade95e3637232c98d4f9d05e03048ba7d22c
Markus Jarvis <markus.h.jarvis@gmail.com>
2023-06-12 11:01:13 -0500

    complete calculator testing

--- Test Driven Development (TDD)/Odin_Work/calculator.test.js
+++ Test Driven Development (TDD)/Odin_Work/calculator.test.js
@@ -5,13 +5,13 @@ test('adding 2 + 3 equals 5', () => {
 })
 
 test('subtracting 2 - 3 equals -1', () => {
-    expect(calculator.add(2, 3)).toBe(-1);
+    expect(calculator.subtract(2, 3)).toBe(-1);
 })
 
-test('adding 2 + 3 equals 5', () => {
-    expect(calculator.add(2, 3)).toBe(5);
+test('dividing 5 / 1 equals 5', () => {
+    expect(calculator.divide(5, 1)).toBe(5);
 })
 
-test('adding 2 + 3 equals 5', () => {
-    expect(calculator.add(2, 3)).toBe(5);
+test('multiplying 2 * 3 equals 6', () => {
+    expect(calculator.multiply(2, 3)).toBe(6);
 })
